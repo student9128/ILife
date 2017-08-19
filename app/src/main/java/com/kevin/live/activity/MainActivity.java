2e80b0058691a40e1af98ad1f451b5fa5c8d2604
student9128 <student9128@gmail.com>
2017-08-19 20:53:12 +0800

    实现了底部导航在切换的时候颜色渐变效果

--- app/src/main/java/com/kevin/live/activity/MainActivity.java
+++ app/src/main/java/com/kevin/live/activity/MainActivity.java
@@ -1,8 +1,12 @@
 package com.kevin.live.activity;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageView;
@@ -24,7 +28,7 @@ import java.util.List;
 import butterknife.ButterKnife;
 
 
-public class MainActivity extends BaseActivity implements TabLayout.OnTabSelectedListener, View.OnClickListener {
+public class MainActivity extends BaseActivity implements TabLayout.OnTabSelectedListener, View.OnClickListener, ViewPager.OnPageChangeListener {
     private LinearLayout mContainer;
     private Toolbar mToolbar;
     private NoSmoothViewPager mViewPager;
@@ -33,11 +37,20 @@ public class MainActivity extends BaseActivity implements TabLayout.OnTabSelecte
     private TabLayoutFragmentAdapter mAdapter;
     private List<Fragment> mFragments = new ArrayList<>();
     private int[] mTabImgs = new int[]{R.drawable.ic_home, R.drawable.ic_news, R.drawable.ic_study, R.drawable.ic_me};
+    private int[] mTabImgsFill = new int[]{R.drawable.ic_home_fill, R.drawable.ic_news_fill,
+            R.drawable.ic_study_fill, R.drawable.ic_me_fill};
 
-    //    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//    }
+    //灰色以及相对应的RGB值
+    private int mGrayColor;
+    private int mGrayRed;
+    private int mGrayGreen;
+    private int mGrayBlue;
+    //灰色以及相对应的RGB值
+    private int mGreenColor;
+    private int mGreenRed;
+    private int mGreenGreen;
+    private int mGreenBlue;
+
     @Override
     public void initView() {
 
@@ -48,7 +61,7 @@ public class MainActivity extends BaseActivity implements TabLayout.OnTabSelecte
         mTabLayout = (TabLayout) findViewById(R.id.tab_layout);
         initTabList();
         initFragmentList();
-        mAdapter = new TabLayoutFragmentAdapter(getSupportFragmentManager(), this, mFragments, mTabList, mTabImgs);
+        mAdapter = new TabLayoutFragmentAdapter(getSupportFragmentManager(), this, mFragments, mTabList, mTabImgs, mTabImgsFill);
         mViewPager.setAdapter(mAdapter);
         mViewPager.setCurrentItem(0);
         mViewPager.setOffscreenPageLimit(4);
@@ -82,12 +95,12 @@ public class MainActivity extends BaseActivity implements TabLayout.OnTabSelecte
 
     @Override
     public void initData() {
-
+        initColor();
     }
 
     @Override
     public void initListener() {
-
+        mViewPager.addOnPageChangeListener(this);
     }
 
     @Override
@@ -109,16 +122,25 @@ public class MainActivity extends BaseActivity implements TabLayout.OnTabSelecte
         View customView = tab.getCustomView();
         TextView tabText = (TextView) customView.findViewById(R.id.tv_tab_text);
         ImageView tabIcon = (ImageView) customView.findViewById(R.id.iv_tab_icon);
+        ImageView tabIconFill = (ImageView) customView.findViewById(R.id.iv_tab_icon_fill);
         tabText.setTextColor(ContextCompat.getColor(this, R.color.colorPrimary));
         String s = tabText.getText().toString();
         if (getString(R.string.tab_home).equals(s)) {
-            tabIcon.setImageResource(R.drawable.ic_home_fill);
+//            tabIcon.setImageResource(R.drawable.ic_home_fill);
+            tabIcon.setColorFilter(mGreenColor, PorterDuff.Mode.SRC_IN);
+            tabIconFill.setAlpha(1f);
         } else if (getString(R.string.tab_performance).equals(s)) {
-            tabIcon.setImageResource(R.drawable.ic_news_fill);
+//            tabIcon.setImageResource(R.drawable.ic_news_fill);
+            tabIcon.setColorFilter(mGreenColor, PorterDuff.Mode.SRC_IN);
+            tabIconFill.setAlpha(1f);
         } else if (getString(R.string.tab_repository).equals(s)) {
-            tabIcon.setImageResource(R.drawable.ic_study_fill);
+            tabIcon.setColorFilter(mGreenColor, PorterDuff.Mode.SRC_IN);
+            tabIconFill.setAlpha(1f);
+//            tabIcon.setImageResource(R.drawable.ic_study_fill);
         } else if (getString(R.string.tab_me).equals(s)) {
-            tabIcon.setImageResource(R.drawable.ic_me_fill);
+            tabIcon.setColorFilter(mGreenColor, PorterDuff.Mode.SRC_IN);
+            tabIconFill.setAlpha(1f);
+//            tabIcon.setImageResource(R.drawable.ic_me_fill);
         }
     }
 
@@ -126,17 +148,20 @@ public class MainActivity extends BaseActivity implements TabLayout.OnTabSelecte
         View customView = tab.getCustomView();
         TextView tabText = (TextView) customView.findViewById(R.id.tv_tab_text);
         ImageView tabIcon = (ImageView) customView.findViewById(R.id.iv_tab_icon);
+        ImageView tabIconFill = (ImageView) customView.findViewById(R.id.iv_tab_icon_fill);
         tabText.setTextColor(ContextCompat.getColor(this, R.color.gray));
         String s = tabText.getText().toString();
-        if (getString(R.string.tab_home).equals(s)) {
-            tabIcon.setImageResource(R.drawable.ic_home);
-        } else if (getString(R.string.tab_performance).equals(s)) {
-            tabIcon.setImageResource(R.drawable.ic_news);
-        } else if (getString(R.string.tab_repository).equals(s)) {
-            tabIcon.setImageResource(R.drawable.ic_study);
-        } else if (getString(R.string.tab_me).equals(s)) {
-            tabIcon.setImageResource(R.drawable.ic_me);
-        }
+        tabIcon.setColorFilter(mGrayColor, PorterDuff.Mode.SRC_IN);
+        tabIconFill.setAlpha(0f);
+//        if (getString(R.string.tab_home).equals(s)) {
+//            tabIcon.setImageResource(R.drawable.ic_home);
+//        } else if (getString(R.string.tab_performance).equals(s)) {
+//            tabIcon.setImageResource(R.drawable.ic_news);
+//        } else if (getString(R.string.tab_repository).equals(s)) {
+//            tabIcon.setImageResource(R.drawable.ic_study);
+//        } else if (getString(R.string.tab_me).equals(s)) {
+//            tabIcon.setImageResource(R.drawable.ic_me);
+//        }
     }
 
     @Override
@@ -146,5 +171,96 @@ public class MainActivity extends BaseActivity implements TabLayout.OnTabSelecte
 //                startActivity(new Intent(this, SearchActivity.class));
 //                break;
         }
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+        int selectedTabPosition = mTabLayout.getSelectedTabPosition();
+        printLogd("Select:\t" + selectedTabPosition);
+        printLogd("Position:\t" + position);
+        ColorStateList tabTextColors = mTabLayout.getTabTextColors();
+        if (positionOffset > 0) {
+            printLogd("Position:===============\t" + position);
+            TabLayout.Tab tabAt = mTabLayout.getTabAt(position);
+            TabLayout.Tab tabAt1 = mTabLayout.getTabAt(position + 1);
+
+            View customView1 = tabAt1.getCustomView();
+            TextView tabText1 = (TextView) customView1.findViewById(R.id.tv_tab_text);
+            ImageView tabIcon1 = (ImageView) customView1.findViewById(R.id.iv_tab_icon);
+            ImageView tabIconFill1 = (ImageView) customView1.findViewById(R.id.iv_tab_icon_fill);
+
+            View customView = tabAt.getCustomView();
+            TextView tabText = (TextView) customView.findViewById(R.id.tv_tab_text);
+            ImageView tabIcon = (ImageView) customView.findViewById(R.id.iv_tab_icon);
+            ImageView tabIconFill = (ImageView) customView.findViewById(R.id.iv_tab_icon_fill);
+            if (positionOffset >= 0.1 && positionOffset <= 0.9) {
+                tabText.setTextColor(get2Gray(positionOffset));
+                tabText1.setTextColor(get2Green(positionOffset));
+            }
+            if (positionOffset < 0.5) {
+                tabIcon.setColorFilter(mGreenColor, PorterDuff.Mode.SRC_IN);
+                tabIcon1.setColorFilter(getGrayToGreen(positionOffset), PorterDuff.Mode.SRC_IN);
+                tabIconFill.setAlpha(1 - 2 * positionOffset);
+                tabIconFill1.setAlpha(0f);
+
+            } else {
+                tabIcon.setColorFilter(getGreenToGray(positionOffset), PorterDuff.Mode.SRC_IN);
+                tabIcon1.setColorFilter(mGreenColor, PorterDuff.Mode.SRC_IN);
+                tabIconFill.setAlpha(0f);
+                tabIconFill1.setAlpha(2 * positionOffset - 1);
+            }
+        }
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+
+    private void initColor() {
+        mGrayColor = ContextCompat.getColor(this, R.color.gray);
+        mGrayRed = Color.red(mGrayColor);
+        mGrayGreen = Color.green(mGrayColor);
+        mGrayBlue = Color.blue(mGrayColor);
+        mGreenColor = ContextCompat.getColor(this, R.color.colorPrimary);
+        mGreenRed = Color.red(mGreenColor);
+        mGreenGreen = Color.green(mGreenColor);
+        mGreenBlue = Color.blue(mGreenColor);
+    }
+
+    private int get2Gray(float offset) {
+        int red = (int) (1.25 * offset * (mGrayRed - mGreenRed) - 0.125 * mGrayRed + 1.125 * mGreenRed);
+        int green = (int) (1.25 * offset * (mGrayGreen - mGreenGreen) - 0.125 * mGrayGreen + 1.125 * mGreenGreen);
+        int blue = (int) (1.25 * offset * (mGrayBlue - mGreenBlue) - 0.125 * mGrayBlue + 1.125 * mGreenBlue);
+        return Color.argb(255, red, green, blue);
+    }
+
+    private int get2Green(float offset) {
+        int red = (int) (1.25 * offset * (mGreenRed - mGrayRed) - 0.125 * mGreenRed + 1.125 * mGrayRed);
+        int green = (int) (1.25 * offset * (mGreenGreen - mGrayGreen) - 0.125 * mGreenGreen + 1.125 * mGrayGreen);
+        int blue = (int) (1.25 * offset * (mGreenBlue - mGrayBlue) - 0.125 * mGreenBlue + 1.125 * mGrayBlue);
+        return Color.argb(255, red, green, blue);
+    }
+
+    private int getGrayToGreen(float positionOffset) {
+        int red = (int) (positionOffset * (mGreenRed - mGrayRed) * 2 + mGrayRed);
+        int green = (int) (positionOffset * (mGreenGreen - mGrayGreen) * 2 + mGrayGreen);
+        int blue = (int) ((positionOffset) * (mGreenBlue - mGrayBlue) * 2 + mGrayBlue);
+//        Log.d("why ", "#### " + red + "  " + green + "  " + blue);
+        return Color.argb(255, red, green, blue);
+    }
+
+    private int getGreenToGray(float positionOffset) {
+        int red = (int) (positionOffset * (mGrayRed - mGreenRed) * 2 + 2 * mGreenRed - mGrayRed);
+        int green = (int) (positionOffset * (mGrayGreen - mGreenGreen) * 2 + 2 * mGreenGreen - mGrayGreen);
+        int blue = (int) (positionOffset * (mGrayBlue - mGreenBlue) * 2 + 2 * mGreenBlue - mGrayBlue);
+//        Log.d("why ", "#### " + red + "  " + green + "  " + blue);
+        return Color.argb(255, red, green, blue);
     }
 }
--- app/src/main/java/com/kevin/live/adapter/TabLayoutFragmentAdapter.java
+++ app/src/main/java/com/kevin/live/adapter/TabLayoutFragmentAdapter.java
@@ -1,6 +1,7 @@
 package com.kevin.live.adapter;
 
 import android.content.Context;
+import android.graphics.PorterDuff;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -24,21 +25,23 @@ public class TabLayoutFragmentAdapter extends FragmentPagerAdapter {
     private Context mContext;
     private List<Fragment> mFragments;
     private List<String> mTabList;
-    private int[] mTabImgs;
+    private int[] mTabImgs, mTabImgsFill;
 
-    private ImageView mTabIcon;
+    private ImageView mTabIcon, mTabIconFill;
     private TextView mTabTitle;
 
     public TabLayoutFragmentAdapter(FragmentManager fm) {
         super(fm);
     }
 
-    public TabLayoutFragmentAdapter(FragmentManager fm, Context context, List<Fragment> fragments, List<String> tabList, int[] tabImgs) {
+    public TabLayoutFragmentAdapter(FragmentManager fm, Context context, List<Fragment> fragments,
+                                    List<String> tabList, int[] tabImgs, int[] tabImgFill) {
         super(fm);
         this.mContext = context;
         this.mFragments = fragments;
         this.mTabList = tabList;
         this.mTabImgs = tabImgs;
+        this.mTabImgsFill = tabImgFill;
     }
 
     @Override
@@ -56,15 +59,23 @@ public class TabLayoutFragmentAdapter extends FragmentPagerAdapter {
         return mTabList.get(position);
     }
 
-    public View getTabView(int position){
+    public View getTabView(int position) {
         View view = LayoutInflater.from(mContext).inflate(R.layout.layout_tab_view, null);
         mTabIcon = (ImageView) view.findViewById(R.id.iv_tab_icon);
         mTabTitle = (TextView) view.findViewById(R.id.tv_tab_text);
+        mTabIconFill = (ImageView) view.findViewById(R.id.iv_tab_icon_fill);
         mTabIcon.setImageResource(mTabImgs[position]);
         mTabTitle.setText(mTabList.get(position));
+        mTabIconFill.setImageResource(mTabImgsFill[position]);
         if (0 == position) {//the default color of item home is green
             mTabTitle.setTextColor(ContextCompat.getColor(mContext, R.color.colorPrimary));
-            mTabIcon.setImageResource(R.drawable.ic_home_fill);
+//            mTabIcon.setImageResource(R.drawable.ic_home_fill);
+            mTabIconFill.setAlpha(1f);
+            mTabIcon.setColorFilter(ContextCompat.getColor(mContext, R.color.colorPrimary), PorterDuff.Mode.SRC_IN);
+        } else {
+//            mTabIcon.setAlpha(0f);
+            mTabIconFill.setAlpha(0f);
+            mTabIcon.setColorFilter(ContextCompat.getColor(mContext, R.color.gray), PorterDuff.Mode.SRC_IN);
         }
         return view;
     }
